2aa87c16625a358268b98533b91be3e449f0e4d6
Agustín Lopini <juanagustin89@gmail.com>
2014-10-20 23:52:53 -0300

    Estilos de contenido y footer
    Footer armado
    Estilos en general del sidebar
    Estilos en general del contenido

--- footer.php
+++ footer.php
@@ -1,24 +1,24 @@
 			<footer class="footer" role="contentinfo">
 
 				<div id="inner-footer" class="wrap cf">
+					
+					<p class="source-org copyright"><a class="footer-logo" href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/library/images/footer-logo.png" alt="Ingeniero White" /></a> Todos los derechos reservados &copy; <?php echo date( 'Y' ); ?>.</p>
 
-<!--					<nav role="navigation">-->
-<!--						--><?php //wp_nav_menu(array(
-//    					'container' => '',                              // remove nav container
-//    					'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
-//    					'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
-//    					'menu_class' => 'nav footer-nav cf',            // adding custom nav class
-//    					'theme_location' => 'footer-links',             // where it's located in the theme
-//    					'before' => '',                                 // before the menu
-//        			'after' => '',                                  // after the menu
-//        			'link_before' => '',                            // before each link
-//        			'link_after' => '',                             // after each link
-//        			'depth' => 0,                                   // limit the depth of the nav
-//    					'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
-//						)); ?>
-<!--					</nav>-->
-
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+					<nav role="navigation">
+						<?php wp_nav_menu(array(
+    					'container' => '',                              // remove nav container
+    					'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
+    					'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
+    					'menu_class' => 'footer-nav cf',            // adding custom nav class
+    					'theme_location' => 'footer-links',             // where it's located in the theme
+    					'before' => '',                                 // before the menu
+	        			'after' => '',                                  // after the menu
+    	    			'link_before' => '',                            // before each link
+        				'link_after' => '',                             // after each link
+        				'depth' => 0,                                   // limit the depth of the nav
+    					'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
+						)); ?>
+					</nav>
 
 				</div>
 
